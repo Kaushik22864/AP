ee944281d5e9938b49bd64a53f12c890f80f0026
Kaushik Joshi <joshirajkaushik@gmail.com>
2025-07-10 14:43:59 +0545

    Delete NepalTourismSystem directory

--- NepalTourismSystem/src/main/java/com/example/nepaltourismsystem/InvalidDateException.java
+++
@@ -1,9 +0,0 @@
-package com.example.nepaltourismsystem;
-
-import java.time.LocalDate;
-
-public class InvalidDateException extends InvalidDataException {
-    public InvalidDateException(LocalDate date) {
-        super("Invalid booking date: " + date);
-    }
-}
