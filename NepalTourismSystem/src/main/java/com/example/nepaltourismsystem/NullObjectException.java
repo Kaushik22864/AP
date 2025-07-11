089dbc1ffb5697fb14e2b557cf867100d8be1c89
Kaushik Joshi <joshirajkaushik@gmail.com>
2025-07-11 08:56:03 +0545

    Delete NepalTourismSystem directory

--- NepalTourismSystem/src/main/java/com/example/nepaltourismsystem/NullObjectException.java
+++
@@ -1,7 +0,0 @@
-package com.example.nepaltourismsystem;
-
-public class NullObjectException extends InvalidDataException {
-    public NullObjectException(String objectName) {
-        super(objectName + " must not be null.");
-    }
-}
