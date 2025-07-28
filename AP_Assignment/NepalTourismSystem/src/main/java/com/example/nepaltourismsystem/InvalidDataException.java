7a89eb436947696722a575b44f29b28caadc3050
Kaushik Joshi <joshirajkaushik@gmail.com>
2025-07-28 19:07:32 +0545

    Delete AP_Assignment directory

--- AP_Assignment/NepalTourismSystem/src/main/java/com/example/nepaltourismsystem/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package com.example.nepaltourismsystem;
-
-public class InvalidDataException extends Exception {
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
