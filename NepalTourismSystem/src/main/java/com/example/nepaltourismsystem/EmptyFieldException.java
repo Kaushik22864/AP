5e50beb93cf3704d41560da26b19b7aebb30c101
Kaushik Joshi <joshirajkaushik@gmail.com>
2025-07-29 16:02:25 +0545

    Delete NepalTourismSystem directory

--- NepalTourismSystem/src/main/java/com/example/nepaltourismsystem/EmptyFieldException.java
+++
@@ -1,7 +0,0 @@
-package com.example.nepaltourismsystem;
-
-public class EmptyFieldException extends InvalidDataException {
-    public EmptyFieldException(String fieldName) {
-        super(fieldName + " cannot be empty.");
-    }
-}
